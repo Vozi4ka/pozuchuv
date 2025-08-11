bc8436183cf9dad64e2a957c988ed6f15bda612e
Vozi4ka <vidravoloda@gmail.com>
2025-08-11 23:00:59 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "7908853006:AAE2lVWuB3vsxUqB_oTdM50oiekZnExfXUk"
+ADMIN_ID = 5417279012
+SENDER_ID = 5417279012
+GPT_PROMPT = "Ты бизнесбот в телеграмм. Ты должке отвечать когда пользователь не в сети. Отвечай не длинными ответами и не форматируй текст.\nЗапрос: "
+BOT_USERNAME = "SendTchekBot"
