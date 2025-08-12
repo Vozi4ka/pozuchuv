bab82decb86228bb828a18b597cabe9c60a9aea2
Vozi4ka <vidravoloda@gmail.com>
2025-08-12 22:00:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "7908853006:AAE2lVWuB3vsxUqB_oTdM50oiekZnExfXUk"
-ADMIN_ID = 6059248768
-SENDER_ID = 6059248768
+ADMIN_ID = 5417279012
+SENDER_ID = 5417279012
 GPT_PROMPT = "Ты бизнесбот в телеграмм. Ты должке отвечать когда пользователь не в сети. Отвечай не длинными ответами и не форматируй текст.\nЗапрос: "
 BOT_USERNAME = "SendTchekBot"
